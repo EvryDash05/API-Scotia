5bf9eb913498839dae28eb46a96c6c8fb3ce417a
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-08-17 21:07:46 -0500

    First commit

---
+++ api-scotia/src/test/java/com/example/api_scotia/ApiScotiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_scotia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiScotiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
